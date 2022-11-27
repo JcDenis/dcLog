17bb4ab0f17b55a0221ba1520543eacc5864cb59
Jean-Christian Denis <contact@jcdenis.fr>
2022-11-27 15:38:40 +0100

    initial commit from dcLog 1.0.1

---
+++ js/dclog.js
@@ -0,0 +1,1 @@
+$(function(){$('.checkboxes-helpers').each(function(){dotclear.checkboxesHelpers(this)});$('input[name="del_logs"]').click(function(){return window.confirm(dotclear.msg.confirm_delete_selected_log)});$('input[name="del_all_logs"]').click(function(){return window.confirm(dotclear.msg.confirm_delete_all_log)})})
