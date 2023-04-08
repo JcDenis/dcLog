87ab340a34133341598b71821cbaac707fe1b13e
Jean-Christian Denis <contact@jcdenis.fr>
2023-04-09 00:34:30 +0200

    fix translation

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -9,22 +9,24 @@
 # DOT NOT MODIFY THIS FILE !
 #
 
-l10n::$locales['Message']                                        = 'Message';
-l10n::$locales['Component']                                      = 'Composant';
-l10n::$locales['User']                                           = 'Utilisateur';
-l10n::$locales['Logs per page']                                  = 'Logs par page';
-l10n::$locales['No log matches the filter']                      = 'Aucun log ne correspond au filtre';
-l10n::$locales['No log']                                         = 'Aucun log';
-l10n::$locales['List of %s logs matching the filter.']           = 'Liste des %s logs correspondants au filtre.';
-l10n::$locales['List of logs. (%s)']                             = 'Liste des logs. (%s)';
-l10n::$locales['All logs have been successfully deleted']        = 'Tous les logs ont été supprimé avec succès';
-l10n::$locales['Selected logs have been successfully deleted']   = 'Les logs sélectionnés ont été supprimé avec succès';
-l10n::$locales['User:']                                          = 'Utilisateur :';
-l10n::$locales['Component:']                                     = 'Composant :';
-l10n::$locales['IP:']                                            = 'IP :';
-l10n::$locales['Are you sure you want to delete selected logs?'] = 'Êtes vous sur de vouloir supprimer les logs sélectionnés?';
-l10n::$locales['Are you sure you want to delete all logs?']      = 'Êtes vous sur de vouloir supprimer tous les logs';
-l10n::$locales['There are no logs']                              = 'Il n\'y a aucun log';
-l10n::$locales['Delete selected logs']                           = 'Supprimer les logs sélectionnés';
-l10n::$locales['Delete all logs']                                = 'Supprimer tous les logs';
-l10n::$locales['Dotclear\'s logs']                               = 'Logs';
+use Dotclear\Helper\L10n;
+
+L10n::$locales['Message']                                        = 'Message';
+L10n::$locales['Component']                                      = 'Composant';
+L10n::$locales['User']                                           = 'Utilisateur';
+L10n::$locales['Logs per page']                                  = 'Logs par page';
+L10n::$locales['No log matches the filter']                      = 'Aucun log ne correspond au filtre';
+L10n::$locales['No log']                                         = 'Aucun log';
+L10n::$locales['List of %s logs matching the filter.']           = 'Liste des %s logs correspondants au filtre.';
+L10n::$locales['List of logs. (%s)']                             = 'Liste des logs. (%s)';
+L10n::$locales['All logs have been successfully deleted']        = 'Tous les logs ont été supprimé avec succès';
+L10n::$locales['Selected logs have been successfully deleted']   = 'Les logs sélectionnés ont été supprimé avec succès';
+L10n::$locales['User:']                                          = 'Utilisateur :';
+L10n::$locales['Component:']                                     = 'Composant :';
+L10n::$locales['IP:']                                            = 'IP :';
+L10n::$locales['Are you sure you want to delete selected logs?'] = 'Êtes vous sur de vouloir supprimer les logs sélectionnés?';
+L10n::$locales['Are you sure you want to delete all logs?']      = 'Êtes vous sur de vouloir supprimer tous les logs';
+L10n::$locales['There are no logs']                              = 'Il n\'y a aucun log';
+L10n::$locales['Delete selected logs']                           = 'Supprimer les logs sélectionnés';
+L10n::$locales['Delete all logs']                                = 'Supprimer tous les logs';
+L10n::$locales['Dotclear\'s logs']                               = 'Logs';
